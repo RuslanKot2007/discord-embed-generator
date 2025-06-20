7d14557391a1f4da3cb05c1afa58a32ba449a899
RuslanKot2007 <166522076+RuslanKot2007@users.noreply.github.com>
2025-06-20 14:22:00 +0300

    fix component index

--- index.js
+++ index.js
@@ -17,14 +17,11 @@
 
 Vue.component('component-form', {
     template: '#component-form-template',
-    props: ['comp', 'parent'],
+    props: ['comp', 'parent', 'idx'],
     computed: {
         uid() {
             return this._uid;
         },
-        idx() {
-            return this.parent.indexOf(this.comp);
-        }
     },
     methods: {
         remove() {
